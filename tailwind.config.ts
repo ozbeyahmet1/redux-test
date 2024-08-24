5a6c10c3a9287f1bbc29ccd2556b22c7a4c670f9
ozbeyahmet1 <ahmet1ozbey@gmail.com>
2024-08-24 07:28:40 +0300

    feat: changes tailwind rules and includes layouts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,19 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/layouts/*",
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      color: {
+        primary: "#2A59FE",
+      },
+      backgroundColor: {
+        primary: "#2A59FE",
       },
     },
   },
