697380e094587196b794d8f0ab0744ad5461d685
ozbeyahmet1 <ahmet1ozbey@gmail.com>
2024-08-24 07:35:21 +0300

    feat: creates logo component

---
+++ ui/components/logo.tsx
@@ -0,0 +1,7 @@
+/**
+ * Renders the Eteration logo.
+ * @returns The JSX element representing the logo.
+ */
+export default function Logo() {
+  return <p className="text-2xl font-extrabold leading-6 text-white">Eteration</p>;
+}
